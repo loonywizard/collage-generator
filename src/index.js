2ed97e1f0cde7b568319bf099d34a19fc196d6ad
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2019-03-23 20:02:10 +0300

    1313123

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 const saveButton = document.getElementById("save");
 
 function downloadImage() {
   console.log("downloadImage");
 }
 
-function generateImageCollage() {}
+async function generateImageCollage() {
+  const image = await fetch(
+    "https://source.unsplash.com/collection/1127163/300x200"
+  );
+  const blob = await image.blob();
+  console.log(blob);
+}
 
 function generateText() {}
 
@@ -15,3 +22,5 @@ function generateCanvasContent() {
 }
 
 saveButton.addEventListener("click", downloadImage);
+
+generateCanvasContent();
