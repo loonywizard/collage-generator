455cf2a93f953fe3f191f02783edea5e92c0d071
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2019-03-23 19:56:07 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("canvas");
+const saveButton = document.getElementById("save");
+
+function downloadImage() {
+  console.log("downloadImage");
+}
+
+function generateImageCollage() {}
+
+function generateText() {}
+
+function generateCanvasContent() {
+  generateImageCollage();
+  generateText;
+}
+
+saveButton.addEventListener("click", downloadImage);
