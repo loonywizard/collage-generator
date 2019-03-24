60e84329154e26d1bdfdd23f0a2e06bbb1bdec88
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2019-03-24 12:16:43 +0300

    image generation ready

--- src/index.js
+++ src/index.js
@@ -2,23 +2,35 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const saveButton = document.getElementById("save");
 
+const UNSPLASH_COLLECTION_URL =
+  "https://source.unsplash.com/collection/1127163/";
+
 function downloadImage() {
   console.log("downloadImage");
 }
 
-async function generateImageCollage() {
-  const image = await fetch(
-    "https://source.unsplash.com/collection/1127163/300x200"
-  );
-  const blob = await image.blob();
-  console.log(blob);
+function generateImage(width, height, left, top) {
+  const image = new Image();
+
+  image.src = `${UNSPLASH_COLLECTION_URL}${width}x${height}`;
+
+  image.onload = () => {
+    ctx.drawImage(image, left, top);
+  };
+}
+
+function generateImageCollage() {
+  generateImage(300, 200, 0, 0);
+  generateImage(200, 200, 300, 0);
+  generateImage(200, 300, 0, 200);
+  generateImage(300, 300, 200, 200);
 }
 
 function generateText() {}
 
 function generateCanvasContent() {
   generateImageCollage();
-  generateText;
+  generateText();
 }
 
 saveButton.addEventListener("click", downloadImage);
